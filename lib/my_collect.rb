0d03963223d98b0a1d50ed56c0678a3e09969113
Muhammad Andriansyah <muh.andriansyah97@gmail.com>
2017-08-11 08:30:55 +0000

    Done.

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -1,2 +1,22 @@
+def my_collect(array)
+  if block_given?
+    collection=Array.new
 
+    i = 0
+    while i < array.length
+      collection[i] = yield(array[i])
+      i = i + 1
+  end
+return collection
 
+else
+  return "Error: No blocks"
+  end
+end
+
+def show_bro
+  collection =['ruby', 'javascript', 'python', 'objective-c']
+  my_collect(collection) do |lang|
+    lang.upcase
+  end
+end
