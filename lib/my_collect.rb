b7dbbb029afdd217c2fce81dd30968fa25039b9f
Kid Coder <toddacodda@gmail.com>
2017-11-28 09:19:20 +0000

    Done.

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -1,2 +1,12 @@
-
-
+def my_collect(array)
+  unless block_given?
+    return "usage: needs a block"
+  end
+  i = 0
+  collection = []
+  while i < array.length
+    collection << yield(array[i])
+    i += 1
+  end
+  collection
+end
