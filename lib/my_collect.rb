112b92d7f746e1d5dc3e88a587dda29a9c27a6cd
abdelrauof mohammed <abdelrauof97@gmail.com>
2018-01-09 19:28:45 +0000

    Done.

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -1,2 +1,14 @@
 
-
+def my_collect(array)
+  i = 0
+  new_array = []
+  while i < array.length
+    new_array << yield(array[i])
+    i += 1 
+  end
+  new_array
+end
+students = ["Tim Jones", "Tom Smith", "Jim Campagno"]
+my_collect(students) { |x|
+  x.split(" ").first
+}
