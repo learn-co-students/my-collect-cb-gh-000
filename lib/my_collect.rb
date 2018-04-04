3768aa17e8eb2fd982a30494965f05cdac7081f0
Anderson Silva <first_stage_1@outlook.com>
2018-04-04 14:14:36 +0000

    Done.

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -1,2 +1,14 @@
-
+def my_collect(array)
+  if block_given?
+    i = 0
+    collection_to_return = []
+    while i < array.length
+      collection_to_return << yield(array[i])
+      i += 1
+    end
+    collection_to_return
+  else
+    # some error -> no block given
+  end
+end
 
