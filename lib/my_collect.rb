2d228faffaa438ec229337214d6bf0b6d50d9d65
Philip Mottershead <philipmottershead@gmail.com>
2018-06-22 21:52:06 +0000

    Done.

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -1,18 +1,10 @@
 def my_collect(collection)
   i=0
-  while i> collection.length
-    yield(collection[i])
+  collect=[]
+
+  while i< collection.length
+    collect.push(yield(collection[i]))
     i+=1
   end
-end
-
-
-array = ["Tim Jones", "Tom Smith", "Jim Campagno"]
-my_collect(array) do |name|
-  name.split(" ").first
-end
-
-collection = ['ruby', 'javascript', 'python', 'objective-c']
-my_collect(collection) do |lang|
-  lang.upcase
+  collect
 end
