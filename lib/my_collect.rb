65e30c4a341f6d4ca9c1da839add0d02c77c4fce
Philip Mottershead <philipmottershead@gmail.com>
2018-06-22 21:46:05 +0000

    Done.

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -1,2 +1,18 @@
+def my_collect(collection)
+  i=0
+  while i> collection.length
+    yield(collection[i])
+    i+=1
+  end
+end
 
 
+array = ["Tim Jones", "Tom Smith", "Jim Campagno"]
+my_collect(array) do |name|
+  name.split(" ").first
+end
+
+collection = ['ruby', 'javascript', 'python', 'objective-c']
+my_collect(collection) do |lang|
+  lang.upcase
+end
